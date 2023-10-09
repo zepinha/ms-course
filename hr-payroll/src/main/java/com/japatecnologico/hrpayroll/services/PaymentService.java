3ea2fc6ea93bce5d7a918ea2f479c8dc3cf60c06
zepinha <japatecnologico@gmail.com>
2023-10-09 19:15:49 +0100

    hr-payroll implementation mock

---
+++ hr-payroll/src/main/java/com/japatecnologico/hrpayroll/services/PaymentService.java
@@ -0,0 +1,13 @@
+package com.japatecnologico.hrpayroll.services;
+
+import org.springframework.stereotype.Service;
+
+import com.japatecnologico.hrpayroll.entities.Payment;
+
+@Service
+public class PaymentService {
+	
+	public Payment getPayment(long workerId, int days) {
+		return new Payment("Bob", 200.0, days);
+	}
+}
